5bcb424741e6b0b92ab701e2741e0297a35a0604
Simon Bailey <simon@pelli.co.uk>
2018-04-24 12:18:01 +0100

    Persist user from local storage token on refresh

--- src/App.js
+++ src/App.js
@@ -2,9 +2,16 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { createStore, compose, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
+import jwtDecode from 'jwt-decode';
+
+// utilities
+import setAuthToken from './utilities/setAuthToken';
 
 // reducers
 import rootReducer from './store/rootReducer';
+
+// actions
+import { setCurrentUser } from './components/Auth/action_creators';
 
 // components
 import Root from './components/Root';
@@ -21,6 +28,11 @@ const store = createStore(
   INITIAL_STATE,
   composeEnhancers(applyMiddleware(thunk)),
 );
+
+if (localStorage.getItem('mm-jwtToken')) {
+  setAuthToken(localStorage.getItem('mm-jwtToken'))
+  store.dispatch(setCurrentUser(jwtDecode(localStorage.getItem('mm-jwtToken'))))
+}
 
 const App = () => (
   <Provider store={store}>
